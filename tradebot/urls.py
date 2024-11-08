6fcfe28f9eed6a4bb998cefb6a9bba69e2d5aefb
anubhav <anubvmonty@gmail.com>
2024-11-08 17:14:36 +0530

    second commit

--- tradebot/urls.py
+++ tradebot/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tradebotapp.urls')),  # Add this line
+    path('', include('tradebotapp.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  # Add this line
 ]
--- tradebotapp/views.py
+++ tradebotapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, welcome to my Django app!")
-
+    return render(request, 'home.html')
 # Create your views here.
+
+def hello(request):
+    return render(request, 'hello.html')
